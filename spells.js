8626a24d023ec79e0587b806e7f017118f0d8005
thedarklord176 <donahueb85@gmail.com>
2022-09-13 14:00:32 -0500

    Added list of spells and images for them

--- spells.js
+++ spells.js
@@ -1,16 +1,36 @@
-function switch_member(){ //change party member
+//boss attacks
+var polarity = document.getElementById("polarity");
+polarity.style.visibility = "hidden";
+var hellsGate = document.getElementById("HellsGate");
+hellsGate.style.visibility = "hidden";
+var spacialRift = document.getElementById("SpacialRift"); //getelementbyid...getelementbyid everywhere
+spacialRift.style.visibility = "hidden";
+var bSun1 = document.getElementById("bSun1");
+bSun1.style.visibility = "hidden";
+var bSun2 = document.getElementById("bSun2");
+bSun2.style.visibility = "hidden";
 
+//Dark mage attacks
+var radiantSupernova = document.getElementById("RadiantSupernova");
+radiantSupernova.style.visibility = "hidden";
+var mirageBlade = document.getElementById("MirageBlade");
+mirageBlade.style.visibility = "hidden";
+var entrapment = document.getElementById("Entrapment");
+entrapment.style.visibility = "hidden";
 
-}
+//warrior attacks
+var thousandMen = document.getElementById("ThousandMen");
+thousandMen.style.visibility = "hidden";
+var shadowSelf = document.getElementById("ShadowSelf");
+shadowSelf.style.visibility = "hidden";
+var whimsofFate = document.getElementById("WhimsofFate");
+whimsofFate.style.visibility = "hidden";
 
-//black mage spells
-function basic_atk(){   
+//light mage attacks
 
-}
-
-//Mage's ultimate attack
-function BlackHoleEclipse(){
-
-
-}
-
+var pierceEvil = document.getElementById("PierceEvil");
+pierceEvil.style.visibility = "hidden";
+var angelsGrace = document.getElementById("AngelsGrace");
+angelsGrace.style.visibility = "hidden";
+var supremeAltar = document.getElementById("SupremeAltar");
+supremeAltar.style.visibility = "hidden";
