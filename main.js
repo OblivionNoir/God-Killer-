d27e538932405e7baec5088dbe49a3a639cf771c
thedarklord176 <donahueb85@gmail.com>
2022-09-12 19:41:05 -0500

    fixed extra newline bug

--- main.js
+++ main.js
@@ -5,15 +5,18 @@ document.addEventListener("keyup", function(event) {
     if (event.code === 'Enter' && check == true) {
         check = false; //so it can only happen once. Prevents event order from getting thrown out of wack
         write_message()
+    }else{
+        console.log("congratulations, you broke something");
     }
 });
 
 
 
 
-function write_message(){ //begin battle when enter is pressed. Write message, delete initial box and make menu appear
+function write_message(){ //begin battle when enter is pressed. Write message and begin battle 
     var player_name = document.getElementById("txt_").value;
-    document.getElementById("txt_").value = `GREETINGS, PATHETIC MORTAL. WHAT WAS YOUR NAME AGAIN? ${player_name.toUpperCase()}? WHATEVER. TIME TO DIE...`;
+    player_name = player_name.toUpperCase()
+    document.getElementById("txt_").value = `GREETINGS, PATHETIC MORTAL. WHAT WAS YOUR NAME AGAIN? ${player_name.replace('\n', '')}? WHATEVER. TIME TO DIE...`;
     setTimeout(() =>{
         window.alert("SHOWTIME!");
         const phase1_theme = new Audio('phase1.mp3')
@@ -24,7 +27,7 @@ function write_message(){ //begin battle when enter is pressed. Write message, d
 
 }
 
-function del_box(){ //START PHASE 1
+function del_box(){ //START PHASE 1. Delete initial box and make menu appear
     var init_box = document.getElementById("txt_");
     init_box.style.display = "none"; 
 
