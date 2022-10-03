0524052a1e644192ad8908a3b64951715ee2093c
thedarklord176 <donahueb85@gmail.com>
2022-10-03 12:58:14 -0500

    Split main into seperate files

---
+++ labeled_bars.js
@@ -0,0 +1,12 @@
+var k_hp = document.getElementById("k_hp").innerHTML = "HP: " + warrior_name_hp.value + "/550";
+var k_mp = document.getElementById("k_mp").innerHTML = "MP: " + warrior_name_mp.value + "/120";
+
+var b_hp = document.getElementById("b_hp").innerHTML = "HP: " + d_mage_name_hp.value + "/470";
+var b_mp = document.getElementById("b_mp").innerHTML = "MP: " + d_mage_name_mp.value + "/260";
+
+var w_hp = document.getElementById("w_hp").innerHTML = "HP: " + l_mage_name_hp.value + "/400";
+var w_mp = document.getElementById("w_mp").innerHTML = "MP: " + l_mage_name_mp.value + "/280";
+
+var u_label = document.getElementById("u_label").innerHTML = ultima_charge.value + "/100";
+
+var boss_label = document.getElementById("boss_hp_label").innerHTML = hp.value + "/15000";
