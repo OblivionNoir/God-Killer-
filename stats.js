9aedfcc8b1dccce6da8e3fa65d935ddbe5becd51
thedarklord176 <donahueb85@gmail.com>
2022-10-09 13:52:27 -0500

    new page to track player stats

---
+++ stats.js
@@ -0,0 +1,28 @@
+var phase_def = 1.2; //divide by this number 
+var phase_mdef = 1.1;
+
+//should turn all these into objects
+var warrior_def = 1.7; 
+var warrior_mdef = 1.4;
+//evasion
+var warrior_ev = 0.05;//5%
+
+
+
+var black_mage_def = 1.3;
+var black_mage_mdef = 1.7;
+var black_mage_ev = 0.1;//10%
+
+
+var white_mage_def = 1.2;
+var white_mage_mdef = 1.8; 
+var white_mage_ev = 0.10;//10%
+
+var red_mage_def = 1.1;
+var red_mage_mdef = 1.2;
+var red_mage_ev = 0.2;//20%
+
+var warrior_dead = false;
+var black_mage_dead = false;
+var white_mage_dead = false;
+var red_mage_dead = false;
