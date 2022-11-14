63f85d562509b5a13f415e757fd75a969e6a9c5f
thedarklord176 <donahueb85@gmail.com>
2022-11-14 17:06:28 -0600

    Made Rebellion spell

--- w_attacks.js
+++ w_attacks.js
@@ -8,40 +8,54 @@
 //low mp cost, quick cooldown
   
   };
-  function Whims_of_Fate(){//low accuracy, but if it lands deals very heavy damage (let's say 4000 base) and lowers boss def
-    x = (4000*warrior_atk)/phase_def
+  function Whims_of_Fate(){//low accuracy, but if it lands deals heavy damage (let's say 3800 base) and lowers boss def by 25% for 2 minutes
+    //defense reduction happens AFTER the damage
+    let k_mp = document.getElementById("warrior_name_mp")
+    //determine if it will hit, 40% chance
+
+    x = (3800*warrior_atk)/phase_def
     hp.value -= x
     console.log(x)
     ending3()
   };
   function Iron_Heart(){
-    //Raises everyone's defenses by 25% for 1 minute
+    //addIcons(knight)
+    //Raises everyone's defenses by 20% for 2 minutes
     let k_mp = document.getElementById("warrior_name_mp")
     if(k_mp.value < 20){
       notEnoughMP()
     }else{
       spellStuff(k_mp, 20, "rebellion.png")
-      RaiseStats()
       Timer(3000, hide_i_and_end)
-    }
-  }
-
-  function RaiseStats(){
-    for([key, value] of defs){
-      value *= 1.25
-      console.log(key, value)
-    }
-    for([key, value] of m_defs){
-      value *= 1.25
-      console.log(key, value)
+      RaiseDefs()//action happens AFTER the image, remember that
     }
   };
-  function RevertStats(){
 
+
+  function RaiseDefs(){
+    for([key, def_val] of defs){
+      defs.set(key, (def_val*1.2).toFixed(2))
+      console.log(defs.get(key, def_val))
+    }
+    for([key, m_def_val] of m_defs){
+      m_defs.set(key, (m_def_val*1.2).toFixed(2))
+      console.log(m_defs.get(key, m_def_val))
+    }
+    Timer(120000, RevertDefs)
+  };
+  function RevertDefs(){//Not sure why I'm getting trailing decimals, but it's not a big deal
+    for([key, def_val] of defs){
+      defs.set(key, (def_val/1.2).toFixed(2))
+      console.log(defs.get(key, def_val))
+    }
+    for([key, m_def_val] of m_defs){
+      m_defs.set(key, (m_def_val/1.2).toFixed(2))
+      console.log(m_defs.get(key, m_def_val))
+    }
   }
 
   function Deathblow(){
-    //Slow and heavy
+    //Slow and heavy, 2 turn attack, 5000 base
 
   }
 
