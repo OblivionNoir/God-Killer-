acb7ecdf2968307a0c92a3d2f7897b9238f55e2b
thedarklord176 <donahueb85@gmail.com>
2022-11-14 17:05:59 -0600

    Increased stat values to 2 decimal places

--- stats.js
+++ stats.js
@@ -1,31 +1,31 @@
-var phase_def = 1.3; //divide by this number 
-var phase_mdef = 1.2;
+var phase_def = 1.30; //divide by this number 
+var phase_mdef = 1.20;
 
-var warrior_def = 1.7; 
-var warrior_mdef = 1.4;
-var warrior_atk = 1.5;
-var warrior_matk = 1;//he doesn't use magic, so it's kinda just for show
+var warrior_def = 1.70; 
+var warrior_mdef = 1.40;
+var warrior_atk = 1.53;
+var warrior_matk = 1.0;//he doesn't use magic, so it's kinda just for show
 var warrior_ev = 0.05;//5%
 
-var black_mage_def = 1.3;
-var black_mage_mdef = 1.5;
-var black_mage_atk = 1.3;
-var black_mage_matk = 1.4;
-var black_mage_ev = 0.1;//10%
+var black_mage_def = 1.30;
+var black_mage_mdef = 1.50;
+var black_mage_atk = 1.34;
+var black_mage_matk = 1.40;
+var black_mage_ev = 0.10;//10%
 
 
-var white_mage_def = 1.2;
-var white_mage_mdef = 1.4; 
-var white_mage_atk = 1;
-var white_mage_matk = 1.4;
+var white_mage_def = 1.18;
+var white_mage_mdef = 1.40; 
+var white_mage_atk = 1.0;
+var white_mage_matk = 1.35;
 var white_mage_ev = 0.10;//10%
 
 
-var red_mage_def = 0.9;
-var red_mage_mdef = 0.9;
-var red_mage_atk = 1.5;
-var red_mage_matk = 1.5;
-var red_mage_ev = 0.2;//20%
+var red_mage_def = 0.90;
+var red_mage_mdef = 0.90;
+var red_mage_atk = 1.52;
+var red_mage_matk = 1.52;
+var red_mage_ev = 0.20;//20%
 
 var defs = new Map([
     ["warrior", warrior_def],
